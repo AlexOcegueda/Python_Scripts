670e148aa8839c43541ff562dbe6fc6655f2f4d3
alexocegueda <alexocegueda59@gmail.com>
2022-01-31 22:58:11 -0600

    Placed Salary entry and text correctly.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from functools import partial
 import tkinter as tk
-from turtle import onclick
 
 def calculate_unadjusted_salary(salary, hours_per_week):
   
@@ -35,35 +34,35 @@ root.geometry("300x300")
 
 
 # Takes in amount of Salary
-salary_label = tk.Label(text="Salary Amount per Hour")
-salary_label.pack()
+salary_label = tk.Label(root, text="Salary Amount per Hour")
+salary_label.place(rely=.25, relx=.30, anchor=tk.CENTER)
 salary_entry = tk.Entry(root)
-salary_entry.pack()
+salary_entry.place(width=100, rely=.25, relx=.75, anchor=tk.CENTER)
 
 
 # Takes in amount of hours per week 
-hours_per_week_label = tk.Label(text="Hours per Week")
-hours_per_week_label.pack()
+hours_per_week_label = tk.Label(root, text="Hours per Week")
+#hours_per_week_label.pack()
 hours_per_week_entry = tk.Entry(root)
-hours_per_week_entry.pack()
+#hours_per_week_entry.pack()
 
 
 # Takes in amount of holiday days off 
-holiday_label = tk.Label(text="Holiday per year")
-holiday_label.pack()
+holiday_label = tk.Label(root, text="Holiday per year")
+#holiday_label.pack()
 holiday_entry = tk.Entry(root)
-holiday_entry.pack()
+#holiday_entry.pack()
 
 
 # Takes in amount of vacation days off
-vacations_days_label = tk.Label(text="Vacation days per year")
-vacations_days_label.pack()
+vacations_days_label = tk.Label(root, text="Vacation days per year")
+#vacations_days_label.pack()
 vacation_days_entry = tk.Entry(root)
-vacation_days_entry.pack()
+#vacation_days_entry.pack()
 
 
 calculate_button = tk.Button(root, text='Calculate', command=lambda: calculate(salary_entry.get(), hours_per_week_entry.get(), holiday_entry.get(), vacation_days_entry.get()))
-calculate_button.pack()
+#calculate_button.pack()
 
 
 root.mainloop()
