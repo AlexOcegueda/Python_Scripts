b7834045a639acca6ab96ad52e6a286d41bf3581
alexocegueda <alexocegueda59@gmail.com>
2022-02-08 21:53:25 -0600

    Removed unecessary parameters in multiple functions

--- temperature_calculator/temp_calc.py
+++ temperature_calculator/temp_calc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from operator import truediv
 import tkinter as tk
 
 '''''
@@ -11,11 +12,22 @@ import tkinter as tk
                             f = c * 9 / 5 + 32
 '''''
 
-# Only returns true 
-def get_valid_float(user_input):
+def get_valid_range():
+
+        
+    if 0 < float(temperature_entry.get()) < 250:
+        return True
+    else:
+        error_message = tk.Label(root, text='Must Enter Between 0-250')
+        error_message.place(relx=.25, rely=.7)
+       
+        
+
+# Only returns true if the 
+def get_valid_float():
     
     try:
-        float(user_input)
+        float(temperature_entry.get())
         return True 
         
     except ValueError:
@@ -23,30 +35,32 @@ def get_valid_float(user_input):
         error_label.place(relx=.25, rely=.7)
 
 
+# converts celsius to fahrenheit
+def convert_to_fahrenheit():
 
-def convert_to_fahrenheit(celsius_input):
-    blank = tk.Label(root, text='         ')
-    blank.place(relx=.2, rely=.6)
+    get_valid_range()
 
-    while get_valid_float(celsius_input):
+    while get_valid_float():
         # this is the fahrenheit formula
-        f = (float(celsius_input) * 9 / 5) + 32
+        f = (float(temperature_entry.get()) * 9 / 5) + 32
         
-        output = (f'Your Fahrenheit temp is {round(f, 1)}')
+        output = (f'Your F temp is {round(f, 1)}')
         f_output_label = tk.Label(root, text=output)
-        f_output_label.place(relx=.2, rely=.6)
+        f_output_label.place(relx=.25, rely=.6)
         break
 
+# converts fahrenheit to Celsius
+def convert_to_celsius():
 
-def convert_to_celsius(fahrenheit_input):
+    if get_valid_range():
 
-    while get_valid_float(fahrenheit_input):
-        # this is the fahrenheit formula
-        c = (float(fahrenheit_input) * 5 / 9) + 32
-        output = (f'Your Celsius temp is {round(c, 1)}')
-        c_output_label = tk.Label(root, text=output)
-        c_output_label.place(relx=.2, rely=.6)
-        break
+        if get_valid_float():
+            # this is the fahrenheit formula
+            c = (float(temperature_entry.get()) * 5 / 9) + 32
+            output = (f'Your C temp is {round(c, 1)}')
+            c_output_label = tk.Label(root, text=output)
+            c_output_label.place(relx=.25, rely=.6)
+           
 
 Font_tuple = ("Comic Sans MS", 23, "bold")
 
@@ -60,14 +74,14 @@ root.geometry("300x250")
 temperature_label = tk.Label(root, text="Enter Temperature:", )
 temperature_label.place(relx=.1, rely=.3)
 temperature_entry = tk.Entry(root, width=3)
-temperature_entry.place(relx=.6, rely=.3)
+temperature_entry.place(relx=.7, rely=.3)
 
 # Button which converts celsius input into fahrenheit
-fahrenheit_label = tk.Button(root, text="C to F", bg='red', fg='white', command=lambda: convert_to_fahrenheit(temperature_entry.get()))
+fahrenheit_label = tk.Button(root, text="C to F", bg='red', fg='white', command=convert_to_fahrenheit)
 fahrenheit_label.place(relx=.2, rely=.4)
 
 # Button which converts fahrenheit input into celsius
-celsius_label = tk.Button(root, text="F to C", bg='blue', fg='white', command=lambda: convert_to_celsius(temperature_entry.get()))
+celsius_label = tk.Button(root, text="F to C", bg='blue', fg='white', command=convert_to_celsius)
 celsius_label.place(relx=.6, rely=.4)
 
 root.mainloop()
