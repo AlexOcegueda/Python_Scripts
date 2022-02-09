9d2522011a0556a32c230d3c998d55199bf8f8f1
alexocegueda <alexocegueda59@gmail.com>
2022-02-08 20:57:32 -0600

    Added title label and placed all widgets.

--- temperature_calculator/temp_calc.py
+++ temperature_calculator/temp_calc.py
@@ -11,23 +11,30 @@ import tkinter as tk
                             f = c * 9 / 5 + 32
 '''''
 
-
+# Only returns true 
 def get_valid_float(user_input):
+    
     try:
         float(user_input)
-        return True
-
+        return True 
+        
     except ValueError:
         error_label = tk.Label(root, text="Must Enter a Number!")
-        error_label.pack()
+        error_label.place(relx=.25, rely=.7)
+
 
 
 def convert_to_fahrenheit(celsius_input):
+    blank = tk.Label(root, text='         ')
+    blank.place(relx=.2, rely=.6)
 
     while get_valid_float(celsius_input):
         # this is the fahrenheit formula
         f = (float(celsius_input) * 9 / 5) + 32
-        print(f)
+        
+        output = (f'Your Fahrenheit temp is {round(f, 1)}')
+        f_output_label = tk.Label(root, text=output)
+        f_output_label.place(relx=.2, rely=.6)
         break
 
 
@@ -36,25 +43,31 @@ def convert_to_celsius(fahrenheit_input):
     while get_valid_float(fahrenheit_input):
         # this is the fahrenheit formula
         c = (float(fahrenheit_input) * 5 / 9) + 32
-        print(c)
+        output = (f'Your Celsius temp is {round(c, 1)}')
+        c_output_label = tk.Label(root, text=output)
+        c_output_label.place(relx=.2, rely=.6)
         break
 
+Font_tuple = ("Comic Sans MS", 23, "bold")
 
 root = tk.Tk()
 root.title("Temperature Converter")
-root.geometry("300x300")
+title_label = tk.Label(root, text='Temperature Converter')
+title_label.configure(font=Font_tuple)
+title_label.place(relx=.03, rely=.1)
+root.geometry("300x250")
 
-temperature_label = tk.Label(root, text="Enter Temperature to Convert ", )
-temperature_label.pack()
-temperature_entry = tk.Entry(root, width=5)
-temperature_entry.pack()
+temperature_label = tk.Label(root, text="Enter Temperature:", )
+temperature_label.place(relx=.1, rely=.3)
+temperature_entry = tk.Entry(root, width=3)
+temperature_entry.place(relx=.6, rely=.3)
 
-# Takes in amount of hours per week
-fahrenheit_label = tk.Button(root, text="Fahrenheit", command=lambda: convert_to_fahrenheit(temperature_entry.get()))
-fahrenheit_label.pack()
+# Button which converts celsius input into fahrenheit
+fahrenheit_label = tk.Button(root, text="C to F", bg='red', fg='white', command=lambda: convert_to_fahrenheit(temperature_entry.get()))
+fahrenheit_label.place(relx=.2, rely=.4)
 
-# Takes in amount of days per week 
-celsius_label = tk.Button(root, text="Celsius", command=lambda: convert_to_celsius(temperature_entry.get()))
-celsius_label.pack()
+# Button which converts fahrenheit input into celsius
+celsius_label = tk.Button(root, text="F to C", bg='blue', fg='white', command=lambda: convert_to_celsius(temperature_entry.get()))
+celsius_label.place(relx=.6, rely=.4)
 
 root.mainloop()
