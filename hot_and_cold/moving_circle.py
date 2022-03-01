1cc872f37698c02824fe152035c55b04725394e1
Alex <alexocegueda59@gmail.com>
2022-03-01 10:44:39 -0600

    renamed variables of screen and turtle.

--- hot_and_cold/moving_circle.py
+++ hot_and_cold/moving_circle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import global_turtle
+import turtle
 
 """
 This module contains functions related to drawing and moving a circle around the screen
@@ -16,7 +16,8 @@ __github__ = "https://github.com/dejohns2/CSC365_Spring2022_Code_Examples"
 x = 0  # center of screen moving right or left
 y = 0  # center of screen moving up or down
 fill_color = 'red'  # the color of the circle
-
+screen = turtle.Screen()
+pen = turtle.Turtle()
 
 def move_home():
     """
@@ -88,7 +89,7 @@ def move_down():
     draw_circle()
 
 
-def setup_window(bg_color='white'):
+def setup_window(pen, bg_color='white'):
     """
     Controls how the window looks.
 
@@ -99,21 +100,21 @@ def setup_window(bg_color='white'):
         None
     """
 
-    global_turtle.wn.tracer(False)  # turn animation off which causes screen flickering as the circle gets redrawn
-    global_turtle.wn.title('Moving Circle')  # title the title bar of the window
-    global_turtle.wn.bgcolor(bg_color)       # set the window's background color
-    global_turtle.wn.setup(800, 900)         # the size of the window
+    pen.wn.tracer(False)  # turn animation off which causes screen flickering as the circle gets redrawn
+    pen.wn.title('Moving Circle')  # title the title bar of the window
+    pen.wn.bgcolor(bg_color)       # set the window's background color
+    pen.wn.setup(800, 900)         # the size of the window
 
     # set up the keys to listen to and what function should be called
-    global_turtle.wn.onkeypress(move_home, "h")
-    global_turtle.wn.onkeypress(move_up, "Up")
-    global_turtle.wn.onkeypress(move_down, "Down")
-    global_turtle.wn.onkeypress(move_right, "Right")
-    global_turtle.wn.onkeypress(move_left, "Left")
-    global_turtle.wn.listen()  # start listening for keys being pressed
+    pen.wn.onkeypress(move_home, "h")
+    pen.wn.onkeypress(move_up, "Up")
+    pen.wn.onkeypress(move_down, "Down")
+    pen.wn.onkeypress(move_right, "Right")
+    pen.wn.onkeypress(move_left, "Left")
+    pen.wn.listen()  # start listening for keys being pressed
 
 
-def draw_circle(diameter=10):
+def draw_circle(pen, diameter=10):
     """
     clear the screen and draw the circle based on the x & y coordinates
 
@@ -123,28 +124,29 @@ def draw_circle(diameter=10):
 
     Returns:
         None
+        :param pen: Turtle which the user controls
     """
 
     global x, y, fill_color
 
-    global_turtle.t.hideturtle()      # don't show the icon
-    global_turtle.t.speed('fastest')  # draw quickly
+    pen.t.hideturtle()      # don't show the icon
+    pen.t.speed('fastest')  # draw quickly
 
-    global_turtle.t.clear()  # clear the previous screen for the update circle location
+    pen.t.clear()  # clear the previous screen for the update circle location
 
     # write text on the screen
-    global_turtle.t.penup()            # don't want to see icon moving on the screen
-    global_turtle.t.goto(-350, 350)    # from the current position which is center after clear, move left 350 up 350
-    global_turtle.t.pencolor('white')  # text color
-    global_turtle.t.write("Use arrows to move, or press 'h' for home", font=("Verdana", 12, "bold"))
+    pen.t.penup()            # don't want to see icon moving on the screen
+    pen.t.goto(-350, 350)    # from the current position which is center after clear, move left 350 up 350
+    pen.t.pencolor('white')  # text color
+    pen.t.write("Use arrows to move, or press 'h' for home", font=("Verdana", 12, "bold"))
 
     # draw circle
-    global_turtle.t.goto(x, y)             # move to the updated x (left-right) and y (up-down) location from center
-    global_turtle.t.pendown()              # start drawing the outline of the circle
-    global_turtle.t.fillcolor(fill_color)  # fill color of the circle
-    global_turtle.t.begin_fill()           # start the fill of whatever is being drawn
-    global_turtle.t.circle(50)             # diameter of the circle
-    global_turtle.t.end_fill()             # done drawing the object to complete the fill
+    pen.t.goto(x, y)             # move to the updated x (left-right) and y (up-down) location from center
+    pen.t.pendown()              # start drawing the outline of the circle
+    pen.t.fillcolor(fill_color)  # fill color of the circle
+    pen.t.begin_fill()           # start the fill of whatever is being drawn
+    pen.t.circle(50)             # diameter of the circle
+    pen.t.end_fill()             # done drawing the object to complete the fill
 
 
 def main():
@@ -154,21 +156,23 @@ def main():
     Returns:
         None
     """
+
     global fill_color
 
     fill_color = 'pink'  # override the default red color
 
-    global_turtle.turtle_setup()  # set up the global window screen & turtle and bring window to front
+    pen.turtle_setup(screen)  # set up the global window screen & turtle and bring window to front
 
     setup_window('black')  # configure how the turtle window screen will look like
 
     draw_circle(50)  # draw the initial shape based on diameter
 
-    global_turtle.wn.mainloop()  # keep the turtle window open until the user closes it
+    pen.wn.mainloop()  # keep the turtle window open until the user closes it
 
-    global_turtle.screen_recreation()  # recreate the window screen so it's ready for the next drawing
+    pen.screen_recreation()  # recreate the window screen so it's ready for the next drawing
 
 
 # if this is the program starting module, then run the main function
 if __name__ == '__main__':
+
     main()
