446a48b42d2c7d42d03bf27e45a87e5394c1555b
Alex <alexocegueda59@gmail.com>
2022-03-01 10:19:39 -0600

    Added base documentation for main.py.

---
+++ hot_and_cold/main.py
@@ -0,0 +1,12 @@
+import turtle
+
+# Place the hidden object randomly on the screen
+# It will begin in the center of the screen
+
+# User determines difficulty of game by controlling size
+
+# Change the objects color depending on whether they are hotter or colder
+
+# Notify them how many moves they made to find the hidden object
+
+# key listeners: s = hidden object H = hides the object R = reset the game
