d8f43720b0bca7dc09d294c6fdb05ccafdef6818
alexocegueda <alexocegueda59@gmail.com>
2022-03-04 18:31:50 -0600

    Removed trail which connected circles and added empty settings box.

--- hot_and_cold/main.py
+++ hot_and_cold/main.py
@@ -1,63 +1,45 @@
-import turtle
+#!/usr/bin/env python3
+
+"""
+This module contains functions related to drawing and moving a circle around the screen
+"""
+
+__author__ = 'Debbie Johnson'
+__version__ = '1.0'
+__copyright__ = "Copyright 2022.02.17, Chapter 4 Assignment"
+__github__ = "https://github.com/dejohns2/CSC365_Spring2022_Code_Examples"
+
 import random
+import turtle
+import tkinter
 
-# This sets the location of the user's coordinates
-user_x = 0  # right left
-user_y = 0  # up down
-
-# This sets the location coordinates of the hidden circle
-hidden_x = 0
-hidden_y = 0
-hidden_color = 'black'
-
-# Player
-pen = None  # Pen == User
 screen = None
-pen_color = 'pink'
+user = None
 
+hidden_x = 100
+hidden_y = 100
+hidden_circle_size = 50
+hidden_circle_color = 'black'
 
-# It will begin in the center of the screen
+user_circle_color = 'pink'
+user_circle_size = 50
 
-# User determines difficulty of game by controlling size
+# position where the turtle will be drawn at
+# these values will change by plus/minus 20 as arrows are pressed
+user_x = 0  # center of screen moving right or left
+user_y = 0  # center of screen moving up or down
+fill_color = 'red'  # the color of the circle
 
-# Change the objects color depending on whether they are hotter or colder
-
-# Notify them how many moves they made to find the hidden object
-
-# key listeners: s = hidden object H = hides the object R = reset the game
 
 def debug():
-    global hidden_color
-    hidden_color = 'white'
-    draw_circle()
-
-
-def set_hidden_location():
     """
-    Generate a random x & y location for the hidden circle based on the default screen size
-    Making sure that the hidden circle isn't too close to the user's circle and
-    abs(x) & abs(y) can't be within twice the user's circle size + 10
-    """
-
-    global hidden_x, hidden_y
-    hidden_x = random.randint(-420, 420)  # left & right max: -420 & 420
-    hidden_y = random.randint(-300, 300)  # bottom & top max: -300 & 300
-
-
-def set_hidden_circle():
-    """
-    Places the hidden object randomly on the screen
-    Returns:
+    Shows where hidden circle is at to make testing easier. Stays up forever.
+    Returns: None
 
     """
-
-    hidden_circle = turtle.Turtle()
-    hidden_circle.goto(hidden_x, hidden_y)
-
-    hidden_circle.fillcolor(hidden_color)
-    hidden_circle.begin_fill()
-    hidden_circle.circle(50)
-    hidden_circle.end_fill()
+    global hidden_circle_color
+    hidden_circle_color = 'white'
+    display_game()
 
 
 def move_home():
@@ -72,48 +54,7 @@ def move_home():
     global user_x, user_y
     user_x = 0  # center of screen moving right or left
     user_y = 0  # center of screen moving up or down
-    draw_circle()
-
-
-def move_up():
-    """
-    Add 20 to the y coordinate which will be used move the circle up
-    then call draw_circle to clear & redraw the circle is on the screen based on its new location
-
-    Returns:
-        None
-    """
-
-    global user_y
-    user_y += 20  # move top of center
-    draw_circle()
-
-
-def move_down():
-    """
-    Subtract 20 from y coordinate which will be used move the circle to the down
-    then call draw_circle to clear & redraw the circle is on the screen based on its new location
-    Returns:
-        None
-    """
-
-    global user_y
-    user_y -= 20  # move down of center
-    draw_circle()
-
-
-def move_right():
-    """
-    Add 20 to the x coordinate which will be used move the circle to the right
-    then call draw_circle to clear & redraw the circle is on the screen based on its new location
-
-    Returns:
-        None
-    """
-
-    global user_x
-    user_x += 20  # move to the right of center
-    draw_circle()
+    display_game()
 
 
 def move_left():
@@ -127,56 +68,61 @@ def move_left():
 
     global user_x
     user_x -= 20  # move to the left of center
-    draw_circle()
+    display_game()
 
 
-def draw_circle():
+def move_right():
     """
-    clear the screen and draw the circle based on the x & y coordinates
+    Add 20 to the x coordinate which will be used move the circle to the right
+    then call draw_circle to clear & redraw the circle is on the screen based on its new location
 
     Returns:
         None
     """
 
-    global user_x, user_y
-
-    pen.hideturtle()  # don't show the icon
-    pen.speed('fastest')  # draw quickly
-
-    pen.clear()  # clear the previous screen for the update circle location
-
-    # write text on the screen
-    pen.penup()  # don't want to see icon moving on the screen
-    pen.goto(-350, 350)  # from the current position which is center after clear, move left 350 up 350
-    pen.pencolor('white')  # text color
-    pen.write("Use arrows to move, or press 'h' for home", font=("Verdana", 12, "bold"))
-
-    set_hidden_circle()
-
-    # draw circle
-    pen.goto(user_x, user_y)  # move to the updated x (left-right) and y (up-down) location from center
-    pen.pendown()  # start drawing the outline of the circle
-    pen.fillcolor(pen_color)  # fill color of the circle
-    pen.begin_fill()  # start the fill of whatever is being drawn
-    pen.circle(50)  # diameter of the circle
-    pen.end_fill()  # done drawing the object to complete the fill
+    global user_x
+    user_x += 20  # move to the right of center
+    display_game()
 
 
-def setup_window(bg_color='white'):
+def move_up():
+    """
+    Add 20 to the y coordinate which will be used move the circle up
+    then call draw_circle to clear & redraw the circle is on the screen based on its new location
+
+    Returns:
+        None
+    """
+
+    global user_y
+    user_y += 20  # move top of center
+    display_game()
+
+
+def move_down():
+    """
+    Subtract 20 from y coordinate which will be used move the circle to the down
+    then call draw_circle to clear & redraw the circle is on the screen based on its new location
+    Returns:
+        None
+    """
+
+    global user_y
+    user_y -= 20  # move down of center
+    display_game()
+
+
+def setup_window():
     """
     Controls how the window looks.
 
-    Args:
-        bg_color (str): the background color of the window (default white)
-
     Returns:
         None
     """
-    screen.tracer(False)  # turn animation off which causes screen flickering as the circle gets redrawn
 
-    # pen.screen.tracer(False)
-    screen.title('Moving Circle')  # title the title bar of the window
-    screen.bgcolor(bg_color)  # set the window's background color
+    screen.tracer(False)  # turn animation off which causes screen flickering as the circle gets redrawn
+    screen.title('Hot or Cold')  # title the title bar of the window
+    screen.bgcolor('black')  # set the window's background color
     screen.setup(800, 900)  # the size of the window
 
     # set up the keys to listen to and what function should be called
@@ -189,6 +135,90 @@ def setup_window(bg_color='white'):
     screen.listen()  # start listening for keys being pressed
 
 
+def set_hidden_location():
+    """
+    Generate a random x & y location for the hidden circle based on the default screen size
+    Making sure that the hidden circle isn't too close to the user's circle and
+    abs(x) & abs(y) can't be within twice the user's circle size + 10
+    """
+
+    global hidden_x, hidden_y
+    hidden_x = random.randint(-420, 420)  # left & right max: -420 & 420
+    hidden_y = random.randint(-300, 300)  # bottom & top max: -300 & 300
+
+
+def draw_instructions():
+    # write text on the screen
+
+    user.goto(-350, 350)  # from the current position which is center after clear, move left 350 up 350
+    user.pencolor('white')  # text color
+    user.write("Use arrows to move, or press 'h' for home", font=("Verdana", 12, "bold"))
+
+
+def draw_hidden_circle():
+    """
+    clear the screen and draw the circle based on the x & y coordinates
+
+    Returns:
+        None
+    """
+
+    user.speed('fastest')  # draw quickly
+
+    # draw circle
+    user.goto(hidden_x, hidden_y)  # move to the updated x (left-right) and y (up-down) location from center
+    user.pendown()  # start drawing the outline of the circle
+    user.fillcolor(hidden_circle_color)  # fill color of the circle
+    user.begin_fill()  # start the fill of whatever is being drawn
+    user.circle(hidden_circle_size)  # diameter of the circle
+    user.end_fill()  # done drawing the object to complete the fill
+
+
+def draw_user_circle():
+    """
+    clear the screen and draw the circle based on the x & y coordinates
+
+    Returns:
+        None
+    """
+
+    user.speed('fastest')  # draw quickly
+
+    # draw circle
+    user.goto(user_x, user_y)  # move to the updated x (left-right) and y (up-down) location from center
+    user.pendown()  # start drawing the outline of the circle
+    user.fillcolor(user_circle_color)  # fill color of the circle
+    user.begin_fill()  # start the fill of whatever is being drawn
+    user.circle(user_circle_size)  # diameter of the circle
+    user.end_fill()  # done drawing the object to complete the fill
+
+
+def display_game():
+    """
+    Returns:
+
+    """
+    user.clear()  # clear the previous screen for the update circle location
+    user.hideturtle()  # don't show the icon
+    user.penup()
+    draw_user_circle()
+    user.penup()  # avoid a trail when drawing next object
+    draw_instructions()
+    user.penup()  # don't want to see icon moving on the screen for next object
+    draw_hidden_circle()
+
+
+def game_menu():
+    root = tkinter.Tk()
+    root.title("Game Settings")
+    root.geometry("300x250")
+
+    title_font = ("Comic Sans MS", 14, "bold")
+    title_label = tkinter.Label(root, text='Game Settings')
+    title_label.configure(font=title_font)
+    title_label.place(relx=.03, rely=.1)
+
+
 def main():
     """
     The main function, used to test drawing a square
@@ -196,15 +226,16 @@ def main():
     Returns:
         None
     """
-    setup_window('black')
-    set_hidden_location()  # Generate random location for hidden turtle
-    draw_circle()
-    set_hidden_circle()
-    turtle.mainloop()
+    game_menu()
+
+    setup_window()  # configure how the turtle window screen will look like
+    set_hidden_location()  # displays a fresh location
+    display_game()  # draws everything on screen according to user input
+    screen.mainloop()  # keep the turtle window open until the user closes it
 
 
 # if this is the program starting module, then run the main function
 if __name__ == '__main__':
     screen = turtle.Screen()
-    pen = turtle.Turtle()
+    user = turtle.Turtle(visible=False)
     main()
--- hot_and_cold/moving_circle.py
+++ hot_and_cold/moving_circle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import turtle
-
 """
 This module contains functions related to drawing and moving a circle around the screen
 """
@@ -11,13 +9,214 @@ __version__ = '1.0'
 __copyright__ = "Copyright 2022.02.17, Chapter 4 Assignment"
 __github__ = "https://github.com/dejohns2/CSC365_Spring2022_Code_Examples"
 
+import random
+import turtle
+import tkinter
+
+screen = None
+user = None
+
+hidden_x = 100
+hidden_y = 100
+hidden_circle_size = 50
+hidden_circle_color = 'black'
+
+user_circle_color = 'pink'
+user_circle_size = 50
+
 # position where the turtle will be drawn at
 # these values will change by plus/minus 20 as arrows are pressed
-x = 0  # center of screen moving right or left
-y = 0  # center of screen moving up or down
+user_x = 0  # center of screen moving right or left
+user_y = 0  # center of screen moving up or down
 fill_color = 'red'  # the color of the circle
-screen = None
-pen = None
+
+
+def debug():
+    """
+    Shows where hidden circle is at to make testing easier. Stays up forever.
+    Returns: None
+
+    """
+    global hidden_circle_color
+    hidden_circle_color = 'white'
+    display_game()
+
+
+def move_home():
+    """
+    Reset the x and y back to zero coordinate which will be used position the circle in the center
+    then call draw_circle to clear & redraw the circle is on the screen based on its new location
+
+    Returns:
+        None
+    """
+
+    global user_x, user_y
+    user_x = 0  # center of screen moving right or left
+    user_y = 0  # center of screen moving up or down
+    display_game()
+
+
+def move_left():
+    """
+    Subtract 20 from the x coordinate which will be used move the circle to the left
+    then call draw_circle to clear & redraw the circle is on the screen based on its new location
+
+    Returns:
+        None
+    """
+
+    global user_x
+    user_x -= 20  # move to the left of center
+    display_game()
+
+
+def move_right():
+    """
+    Add 20 to the x coordinate which will be used move the circle to the right
+    then call draw_circle to clear & redraw the circle is on the screen based on its new location
+
+    Returns:
+        None
+    """
+
+    global user_x
+    user_x += 20  # move to the right of center
+    display_game()
+
+
+def move_up():
+    """
+    Add 20 to the y coordinate which will be used move the circle up
+    then call draw_circle to clear & redraw the circle is on the screen based on its new location
+
+    Returns:
+        None
+    """
+
+    global user_y
+    user_y += 20  # move top of center
+    display_game()
+
+
+def move_down():
+    """
+    Subtract 20 from y coordinate which will be used move the circle to the down
+    then call draw_circle to clear & redraw the circle is on the screen based on its new location
+    Returns:
+        None
+    """
+
+    global user_y
+    user_y -= 20  # move down of center
+    display_game()
+
+
+def setup_window():
+    """
+    Controls how the window looks.
+
+    Returns:
+        None
+    """
+
+    screen.tracer(False)  # turn animation off which causes screen flickering as the circle gets redrawn
+    screen.title('Hot or Cold')  # title the title bar of the window
+    screen.bgcolor('black')  # set the window's background color
+    screen.setup(800, 900)  # the size of the window
+
+    # set up the keys to listen to and what function should be called
+    screen.onkeypress(debug, "d")
+    screen.onkeypress(move_home, "h")
+    screen.onkeypress(move_up, "Up")
+    screen.onkeypress(move_down, "Down")
+    screen.onkeypress(move_right, "Right")
+    screen.onkeypress(move_left, "Left")
+    screen.listen()  # start listening for keys being pressed
+
+
+def set_hidden_location():
+    """
+    Generate a random x & y location for the hidden circle based on the default screen size
+    Making sure that the hidden circle isn't too close to the user's circle and
+    abs(x) & abs(y) can't be within twice the user's circle size + 10
+    """
+
+    global hidden_x, hidden_y
+    hidden_x = random.randint(-420, 420)  # left & right max: -420 & 420
+    hidden_y = random.randint(-300, 300)  # bottom & top max: -300 & 300
+
+
+def draw_instructions():
+    # write text on the screen
+
+    user.goto(-350, 350)  # from the current position which is center after clear, move left 350 up 350
+    user.pencolor('white')  # text color
+    user.write("Use arrows to move, or press 'h' for home", font=("Verdana", 12, "bold"))
+
+
+def draw_hidden_circle():
+    """
+    clear the screen and draw the circle based on the x & y coordinates
+
+    Returns:
+        None
+    """
+
+    user.speed('fastest')  # draw quickly
+
+    # draw circle
+    user.goto(hidden_x, hidden_y)  # move to the updated x (left-right) and y (up-down) location from center
+    user.pendown()  # start drawing the outline of the circle
+    user.fillcolor(hidden_circle_color)  # fill color of the circle
+    user.begin_fill()  # start the fill of whatever is being drawn
+    user.circle(hidden_circle_size)  # diameter of the circle
+    user.end_fill()  # done drawing the object to complete the fill
+
+
+def draw_user_circle():
+    """
+    clear the screen and draw the circle based on the x & y coordinates
+
+    Returns:
+        None
+    """
+
+    user.speed('fastest')  # draw quickly
+
+    # draw circle
+    user.goto(user_x, user_y)  # move to the updated x (left-right) and y (up-down) location from center
+    user.pendown()  # start drawing the outline of the circle
+    user.fillcolor(user_circle_color)  # fill color of the circle
+    user.begin_fill()  # start the fill of whatever is being drawn
+    user.circle(user_circle_size)  # diameter of the circle
+    user.end_fill()  # done drawing the object to complete the fill
+
+
+def display_game():
+    """
+    Returns:
+
+    """
+    user.clear()  # clear the previous screen for the update circle location
+    user.hideturtle()  # don't show the icon
+    user.penup()
+    draw_user_circle()
+    user.penup()  # avoid a trail when drawing next object
+    draw_instructions()
+    user.penup()  # don't want to see icon moving on the screen for next object
+    draw_hidden_circle()
+
+
+def game_menu():
+    root = tkinter.Tk()
+    root.title("Game Settings")
+    root.geometry("300x250")
+
+    title_font = ("Comic Sans MS", 14, "bold")
+    title_label = tkinter.Label(root, text='Game Settings')
+    title_label.configure(font=title_font)
+    title_label.place(relx=.03, rely=.1)
 
 
 def main():
@@ -27,26 +226,16 @@ def main():
     Returns:
         None
     """
+    game_menu()
 
-    global fill_color
-
-    fill_color = 'pink'  # override the default red color
-
-    pen.turtle_setup()  # set up the global window screen & turtle and bring window to front
-
-    setup_window('black')  # configure how the turtle window screen will look like
-
-    draw_circle(50)  # draw the initial shape based on diameter
-
-    pen.screen.mainloop()  # keep the turtle window open until the user closes it
-
-    pen.screen_recreation()  # recreate the window screen so it's ready for the next drawing
+    setup_window()  # configure how the turtle window screen will look like
+    set_hidden_location()  # displays a fresh location
+    display_game()  # draws everything on screen according to user input
+    screen.mainloop()  # keep the turtle window open until the user closes it
 
 
 # if this is the program starting module, then run the main function
 if __name__ == '__main__':
-    global screen, pen
-
     screen = turtle.Screen()
-    pen = turtle.Turtle
+    user = turtle.Turtle(visible=False)
     main()
