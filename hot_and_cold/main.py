6b1618030af2fab4fcca36afb2d2677e5f5f9d4b
alexocegueda <alexocegueda59@gmail.com>
2022-03-04 20:31:55 -0600

    Added move limit logic and fixed user input intake at beginning of new game.

--- hot_and_cold/main.py
+++ hot_and_cold/main.py
@@ -4,32 +4,44 @@
 This module contains functions related to drawing and moving a circle around the screen
 """
 
-__author__ = 'Debbie Johnson'
-__version__ = '1.0'
+__author__ = 'Debbie Johnson, Alex Ocegueda'
+__version__ = '2.0'
 __copyright__ = "Copyright 2022.02.17, Chapter 4 Assignment"
 __github__ = "https://github.com/dejohns2/CSC365_Spring2022_Code_Examples"
 
 import random
 import turtle
-import tkinter
 
 screen = None
 user = None
 
-hidden_x = 100
-hidden_y = 100
-hidden_circle_size = 50
-hidden_circle_color = 'black'
+hidden_x = 100  # x-coord of hidden circle
+hidden_y = 100  # y-coord of hidden circle
+hidden_circle_size = 50  # sets how difficult it is to find
+hidden_circle_color = 'black'  # set to black to be invisible
 
-user_circle_color = 'pink'
-user_circle_size = 50
+user_circle_color = 'pink'  # user circle is default to pink until it gets hot or cold
+user_circle_size = 10  # sets the size of the circle - this affects difficulty.
+move_size = None  # number of steps taken by user
+move_limit = 20  # number of steps limited before game over - set by user also.
 
 # position where the turtle will be drawn at
 # these values will change by plus/minus 20 as arrows are pressed
 user_x = 0  # center of screen moving right or left
 user_y = 0  # center of screen moving up or down
-previous_x = 0
-previous_y = 0
+previous_x = 0  # previous location of user's x coord
+previous_y = 0  # previous location of user's y coord
+
+
+def reset_game():
+    """
+    Resets the entire game by calling main again and resetting the hiddle circle color.
+    Returns:
+
+    """
+    global hidden_circle_color
+    hidden_circle_color = 'black'
+    main()
 
 
 def debug():
@@ -114,6 +126,12 @@ def move_down():
 
 
 def check_hot_or_cold():
+    """
+    This checks the user's location compared to its previous location and
+    sees which one is closer to hidden circle. Depending which one is closer
+    will determine if the circle is 'hot' (red) or 'cold' (blue).
+
+    """
     global previous_x, previous_y, user_circle_color, hidden_circle_color
 
     overlap = user_circle_size * 2 - 10
@@ -126,16 +144,30 @@ def check_hot_or_cold():
             if abs(previous_x - hidden_x) > abs(user_x - hidden_x):
                 user_circle_color = 'red'
             else:
-                user_color = 'blue'
+                user_circle_color = 'blue'
 
         if previous_y != user_y:
             if abs(previous_y - hidden_y) > abs(user_y - hidden_y):
-                user_color = 'red'
+                user_circle_color = 'red'
             else:
-                user_color = 'blue'
+                user_circle_color = 'blue'
 
     previous_x = user_x
     previous_y = user_y
+
+
+def check_move_limit():
+    """
+    Checks moves compared to the limit. If it has reached the cap it will display it and inform
+    the user to reset the game when they are ready to play again.
+    """
+    global move_size
+
+    if move_size >= move_limit:
+        user.goto(-150, 325)
+        user.write("CAP REACHED! Pressed R to reset!", font=("Verdana", 12, "bold"))
+    else:
+        move_size += 1
 
 
 def setup_window():
@@ -152,6 +184,7 @@ def setup_window():
     screen.setup(800, 900)  # the size of the window
 
     # set up the keys to listen to and what function should be called
+    screen.onkeypress(reset_game, "r")
     screen.onkeypress(debug, "d")
     screen.onkeypress(move_home, "h")
     screen.onkeypress(move_up, "Up")
@@ -187,20 +220,43 @@ def set_user_location():
     user_y = center_pos
 
 
+def set_user_settings():
+    global hidden_circle_size, user_circle_size, move_limit, move_size
+    move_size = -1
+
+    try:
+        user_circle_size = int(turtle.numinput('Circle', 'Size of circles (10-100)'))
+        hidden_circle_size = user_circle_size
+    except:
+        user_circle_size = 50
+        hidden_circle_size = 50
+
+    try:
+        move_limit = int(turtle.numinput('Circle', 'Size of move (10-100)'))
+    except:
+        move_limit = 50
+
+
 def draw_instructions():
     # write text on the screen
-
+    user.penup()
     user.goto(-350, 350)  # from the current position which is center after clear, move left 350 up 350
     user.pencolor('white')  # text color
-    user.write("Use arrows to move, or press 'h' for home", font=("Verdana", 12, "bold"))
+    user.write("Use arrows to move", font=("Verdana", 12, "bold"))
+    user.goto(-350, 325)  # from the current position which is center after clear, move left 350 up 350
+    user.write("R = Reset game", font=("Verdana", 12, "bold"))
+    user.goto(-350, 300)  # from the current position which is center after clear, move left 350 up 350
+    user.write("D = Debug/Show hidden", font=("Verdana", 12, "bold"))
+    user.goto(-350, 275)  # from the current position which is center after clear, move left 350 up 350
+    user.write("H = Return circle to center", font=("Verdana", 12, "bold"))
+    user.goto(275, 350)  # from the current position which is center after clear, move left 350 up 350
+    user.write("Moves: " + str(move_size), font=("Verdana", 12, "bold"))
 
 
 def draw_hidden_circle():
     """
     clear the screen and draw the circle based on the x & y coordinates
 
-    Returns:
-        None
     """
 
     user.speed('fastest')  # draw quickly
@@ -218,8 +274,6 @@ def draw_user_circle():
     """
     clear the screen and draw the circle based on the x & y coordinates
 
-    Returns:
-        None
     """
 
     user.speed('fastest')  # draw quickly
@@ -235,42 +289,32 @@ def draw_user_circle():
 
 def display_game():
     """
-    Returns:
+    Displays everything needed after a key was pressed by the user.
 
     """
     user.clear()  # clear the previous screen for the update circle location
+
     user.hideturtle()  # don't show the icon
-    user.penup()
+    check_hot_or_cold()
+    check_move_limit()
+    user.pencolor('black')
+    user.penup()  # avoid a trail when drawing next object
     draw_user_circle()
     user.penup()  # avoid a trail when drawing next object
-    draw_instructions()
-    user.penup()  # don't want to see icon moving on the screen for next object
     draw_hidden_circle()
-
-
-def game_menu():
-    root = tkinter.Tk()
-    root.title("Game Settings")
-    root.geometry("300x250")
-
-    title_font = ("Comic Sans MS", 14, "bold")
-    title_label = tkinter.Label(root, text='Game Settings')
-    title_label.configure(font=title_font)
-    title_label.place(relx=.03, rely=.1)
+    draw_instructions()
+    user.penup()  # # avoid a trail when drawing next object
 
 
 def main():
     """
     The main function, used to test drawing a square
 
-    Returns:
-        None
     """
-    # game_menu()
-
+    set_user_settings()
     setup_window()  # configure how the turtle window screen will look like
-    set_user_location()
-    set_hidden_location()  # displays a fresh location
+    set_user_location()  # sets user's circle to the center based on difficulty selected
+    set_hidden_location()  # displays a fresh hidden location
     display_game()  # draws everything on screen according to user input
     screen.mainloop()  # keep the turtle window open until the user closes it
 
